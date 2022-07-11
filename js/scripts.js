75d66a78745d35840d6e919a353155f017df1a24
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-07-11 00:05:11 -0700

    Update scripts.js
    added comments and pseudocode, deleted unnecessary code and edited functions

--- js/scripts.js
+++ js/scripts.js
@@ -35,30 +35,19 @@ console.log(gallery); //returns the individual employee card info and their inde
 // That data returned needs to be cleaned up and converted into a format we use, so we take the response and run the json() method on the data. -DONE
 // Then we grab that new, more accessible dataset and start working with it! -DONE
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => showEmployees(data.results)); //running the show employee function - this shows them on the webpage
-
 //Function to return fetch method
 function fetchData(url) {   
   return fetch(url) 
     .then(checkStatus) //if fetch promise fulfilled, it will check status of response
     .then((res) => res.json()) //if resolved.then method parses response to json
     .then((data) => {
-      employeeData = data.results
+      employeeData = data.results //this gets saved to the empty array variable
+      showEmployees(employeeData) //running the show employee function - this shows them on the webpage. The employee data which has the data.results will be passed in here- the div elements must match the employees on screen and now they do
     }) //fetchData func will return promise once data is retrieved and parsed to json. populates an array of employees and info
     .catch((error) => console.log("Sorry, something went wrong", error)); //This error message was printed at point to console when I clicked on an employee card, but not currently.
 }
 
 fetchData(url) //when I type this into console it shows promise is "pending", the promiseState is "fulfilled" and promise Result is 'undefined'
-
-function fetchSelectedEmployee() {
-    const selectedEmployee = selectedEmployee.value;  
-    const img = document.querySelector('img');
-    const p = document.querySelector('p');
-
-    fetchData('https://randomuser.me/api/?results=12&nat=us')
-}
 
 //----------------------------------------------
 //HELPER FUNCTIONS
@@ -70,11 +59,17 @@ function checkStatus(response) {
       return Promise.reject(new Error(response.statusText));
     }
   }
-// create a function that takes in an array of employees
-//assign an index so each card is assigned a number to loop through
+// create a function that takes in an array of employees DONE
+//assign an index so each card is assigned a number to loop through DONE I THINK
+//we want js to grab the data-index assigned to a card when we click on it and put it into employeeData array and grab what we clicked and then it loads the modal grabbing that particular index card. 
+//use 'data-index' to grab the clicked item to show in modal
 function showEmployees(data) {
   let employeeCard = data.map((employee, index) => {
-    const html = `<div class="card" index = "${index}">
+    console.log(employee);//returns employee card info
+    console.log(typeof data-index) //returns NaN
+    console.log(typeof employeeData);//returns object
+    console.log(index)//returns a number of the card
+    const html = `<div class="card" data-index="${index}">
     <div class="card-img-container">
       <img class="card-img" src="${employee.picture.medium}" alt="profile picture">  
     </div>
@@ -92,12 +87,36 @@ function showEmployees(data) {
 }
 
 //maybe look at what I'm passing here and try to specify the clicked employee rather than all.
-function getEmployeeCard (data, index) {
+//don't attach event listener to every card, attach it to the overall gallery
+//then "if it's a card, load modal"
+function getEmployeeCard (employeeData) {
+  //listen for a click for the gallery - DONE
+  //check to see if event target was an element by class name 'card' DONE
+  //if so, grab data-index value property (google data attribute html, click on first one, js access, 'article.dataset.columns' is how to grab index property value)DONE
+  //make sure value is an integer so we can use as an index for the employeeData
+  //use that index value to grab the correct employee from employeeData
+  //pass selected employeeData (should be just one employee) to showModal function
+  
+  //syntax: e.target.something (maybe try card) to return value you want to capture
+  //console.log that value adn then try to log out the employee data for just that user
+  //don't do anything with the data until have it isolated
+  //when you create your employee cards in the gallery, loop over teh array of user data. 
+  //create a card for the employee at index 0 first adn then the employee at index 1, then index 2 etc
+  //everytime you create an employee card(when whole gallery is populated) try adding their index value into the card
+  //this will give you a fixed reference to the index for every employee attached to the card.
+  //in the end - should be that when you click  on card in gallery you should console log the index
+  //console log output should be 1 or 2 or 3
+  //once you have that index number you need to access the full employee record from the array. 
+  //click on 3rd employee card(which would be index 2) it logs 2 to the console. 
+  //then you can update your console log statement to say console.log(employees[2]) and log out full dataset for the employee at the that index.
 const cardArray = document.getElementsByClassName("card");
-for (let i= 0; i<cardArray.length; i++) {
-  cardArray[i].addEventListener("click", (e) => {
-    showModal(data) = cardArray[i]; //this shows the modal window popup
-    //showModal(data)
+console.log(cardArray);
+for (let i= 0; i<employeeData[i].length; i++) {
+  gallery.addEventListener("click", (e) => {
+    if(e.target.cardArray) {
+    showModal(cardArray); //this shows the modal window popup
+    //console.log(employeeData);
+    }
   } )
 }
 }
@@ -106,27 +125,6 @@ for (let i= 0; i<cardArray.length; i++) {
 //EVENT LISTENERS
 //----------------------------------------------
 
-// gallery.addEventListener("click", (e) => {
-//   if (e.target.className !== "gallery"){
-//       gallery(e.target.index); 
-//       console.log("button clicked");
-//   }
-// }); //this button does not work...can't seem to get the right names added
-
-//gallery.addEventListener('change', index); 
-//gallery.addEventListener('click', gallery); //this triggered the checkStatus message at one point on console and button did not give what i wanted - when I had fetch Data in there.
-// //None of the variables I pass in the second spot seem to be giving anything or allowing me to move forward 
-
-
-// html.addEventListener("click", (e) => {
-//                     if (e.target.className === "card"){
-//                         showModal(employeeData); //not sure what to pass in ()
-//                     }
-//                 }); //this button does not work
-        
-// document.getElementById('gallery').addEventListener("click", (e) => {
-//     showModal();
-// });    //doesn't work
 
 // NOTE: When adding or concatenating to the DOM, avoid doing this: element.innerHTML += 'HTML string'. That technique rewrites the entire DOM. This is problematic because any rewritten elements won't retain any event listeners that were attached to the DOM before the rewrite occurs. Use this method instead: element.insertAdjacentHTML('beforeend', 'HTML string'). That technique will allow you to add strings of HTML to the DOM without disrupting what already exists in the DOM.
 
@@ -145,7 +143,13 @@ for (let i= 0; i<cardArray.length; i++) {
 // loop over each of the employees
 // for each employee generate the .card html
 
- function showModal(data) {
+//okay to comment this section out while working on other sections
+ function showModal(cardArray) { //problem is passing all data instead of just one
+
+  //data received should be 1 employee object not all in parentheses for function
+  //use the object to populate the modalContainer html
+  //not going to use forEach
+  //append to modalContainer which I already did in insertAdjacentHTML below
      let divCard = data.forEach((employee, index) => {
       //console gives an error on array.forEach   
         
@@ -172,17 +176,18 @@ for (let i= 0; i<cardArray.length; i++) {
                 // append that html above to the #gallery container
                 document.body.insertAdjacentHTML('beforeend', modalContainer) //If I comment this line out - the modal window will not open
               //if I change "body" to gallery it stops popping up the window.
-              const closeButton = document.getElementById("modal-close-btn.modal-close-btn")
+              const closeButton = document.getElementById("modal-close-btn")
                 //creating an event listener to close the modal  - it's not working yet.
-                
-                closeButton[index].addEventListener("click", (e) => {   
+                console.log(closeButton);
+                closeButton.addEventListener("click", (e) => {   
+                //remove or hide the modalContainer
 
                   //putting the word 'button', 'closeButton' in front made the person in "first position" appear in modal but if I click another it still only shows the first person!
                   //if I try to add [i] or index in attempt to match what I clicked it comes up as undefined.
                   //says cannot ready prop of null (reading '0') in the closeButton[index]
                   
                   
-                  "chosenEmployee" //{   //console says e.target is not a function when I click a card
+                  //"chosenEmployee" //{   //console says e.target is not a function when I click a card
 
                       //for(let i=0; i <modal.length; i++) {
               console.log("button clicked"); //this was (not now) working when I click on an employee card in directory - it says "button clicked on console"
